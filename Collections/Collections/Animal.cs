8cce917cc9b98b02874080641918017b995fa599
menchio <rodrigo.filho@g.globo>
2023-09-11 12:41:20 -0300

    List

--- Collections/Collections/Animal.cs
+++ Collections/Collections/Animal.cs
@@ -1,12 +1,19 @@
 ﻿namespace Collections
 {
-	public class Animal
-	{
+	public class Animal : IComparable
+    {
         public string Name { get; set; }
         public int Age { get; set; }
         public float Weight { get; set; }
 
-		override
+        public int CompareTo(object obj)
+        {
+            Animal that = (Animal)obj;
+
+            return Age.CompareTo(that.Age);
+        }
+
+        override
 		public string ToString()
 		{
 			return string.Format("Nome: {0}, Idade: {1}, Peso: {2}", Name, Age, Weight);
--- Collections/Collections/Program.cs
+++ Collections/Collections/Program.cs
@@ -4,12 +4,17 @@ class Program
 {
     public static void Main(string[] args)
     {
-        #region Arrays
+        #region Objects
 
         Animal gato = new() { Name = "Gato", Age = 10, Weight = 15.0f };
         Animal cachorro = new() { Name = "Cachorro", Age = 12, Weight = 25.0f };
-        Animal papagaio = new() { Name = "Papagaio", Age = 32, Weight = 5.0f };
+        Animal papagaio = new() { Name = "Rodolfo", Age = 32, Weight = 5.0f };
 
+        #endregion
+
+        #region Arrays
+
+        /*
         Animal[] arrayAnimal = new Animal[5]
         {
             gato,
@@ -89,6 +94,78 @@ class Program
         {
             Console.WriteLine(animal);
         }
+        */
+
+        #endregion
+
+        #region Lists
+
+        List<Animal> listAnimals = new()
+        {
+            gato,
+            cachorro
+        };
+
+        foreach (var animal in listAnimals)
+        {
+            //Console.WriteLine(animal);
+        }
+
+        listAnimals.Add(papagaio);
+
+        foreach (var animal in listAnimals)
+        {
+            //Console.WriteLine(animal);
+        }
+
+        //listAnimals.AddRange(new List<Animal>{ gato, gato, cachorro});
+
+        foreach (var animal in listAnimals)
+        {
+            //Console.WriteLine(animal);
+        }
+
+        //Console.WriteLine("Mostrando animal com nome de Rodolfo");
+
+        //Console.WriteLine(listAnimals.FirstOrDefault(a => a.Name.Equals("Rodolfo")));
+
+        //Console.WriteLine("Ordenando...");
+
+        listAnimals = listAnimals.OrderBy(a => a.Name).ToList();
+
+        foreach (var animal in listAnimals)
+        {
+            //Console.WriteLine(animal);
+        }
+
+        //Console.WriteLine("Soma das idades: {0}", listAnimals.Sum(a => a.Age));
+
+        Console.WriteLine("Lista ordenada por nome");
+
+        foreach (var animal in listAnimals)
+        {
+            Console.WriteLine(animal);
+        }
+
+        Console.WriteLine("Ordenando por idade...");
+
+        listAnimals.Sort((x, y) => x.Age.CompareTo(y.Age));
+
+        foreach (var animal in listAnimals)
+        {
+            Console.WriteLine(animal);
+        }
+
+        Console.WriteLine("Ordenando por peso...");
+
+        listAnimals.Sort((x, y) => x.Weight.CompareTo(y.Weight));
+
+        foreach (var animal in listAnimals)
+        {
+            Console.WriteLine(animal);
+        }
+
+        Console.ReadLine();
 
         #endregion
     }
