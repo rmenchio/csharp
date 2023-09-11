48251c30d483cc5be4580afaedb1869664dfa289
menchio <rodrigo.filho@g.globo>
2023-09-11 11:58:42 -0300

    testes com data

---
+++ Date/Program.cs
@@ -0,0 +1,14 @@
+﻿DateTime data1 = new DateTime(2000, 10, 10, 2, 15, 10);
+DateTime data2 = new DateTime(2001, 10, 10, 10, 10, 10);
+
+bool compDatas = data1 < data2;
+
+//Console.WriteLine("Data 1 é menor que data 2: {0}", compDatas);
+
+
+bool compHoras = data1.TimeOfDay < data2.TimeOfDay;
+
+//Console.WriteLine("Hora 1 é menor que hora 2: {0}", compHoras);
+
+Console.WriteLine(data1.AddHours(-3).TimeOfDay);
+Console.ReadLine();
