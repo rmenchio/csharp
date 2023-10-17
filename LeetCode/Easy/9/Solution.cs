92cc2d321ab27215a1067c761cf40f740ef9907b
rmenchio <rodrigomenchio@id.uff.br>
2023-10-17 18:35:56 -0300

    leetcode - problema 9

---
+++ LeetCode/Easy/9/Solution.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * Given an integer x, return true if x is a palindrome, and false otherwise.
+ */
+
+namespace LeetCode.Easy.Nine
+{
+	public class Solution
+	{
+        public bool IsPalindrome(int x)
+        {
+            char[] x_list = x.ToString().ToArray();
+
+            Array.Reverse(x_list);
+
+            string x_reverse_string = new string(x_list);
+            string x_string = x.ToString();
+
+            return x_string.Equals(x_reverse_string);
+        }
+    }
+}
---
+++ LeetCode/Program.cs
@@ -0,0 +1,9 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        var question_two = new LeetCode.Easy.Nine.Solution();
+
+        question_two.IsPalindrome(121);
+    }
+}
