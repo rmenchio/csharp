a93c3ab4b8a50a11b45aa408637d972121a78d7b
rmenchio <rodrigomenchio@id.uff.br>
2024-01-03 18:57:26 -0300

    primeira versão

---
+++ LeetCode/Easy/26/Solution.cs
@@ -0,0 +1,41 @@
+﻿namespace LeetCode.Easy._26
+{
+	public class Solution
+	{
+        int qntUnico = 0;
+        List<int> numerosUnicos = new();
+
+        public int RemoveDuplicates(int[] nums)
+        {
+            int contador = 0;
+
+            for (int j = 0; j < nums.Length; j = j)
+            {
+                int num = nums[j];
+
+                if (!numerosUnicos.Contains(num))
+                {
+                    qntUnico++;
+                    numerosUnicos.Add(num);
+                    j++;
+                }
+                else
+                {
+                    for (int i = j; i < nums.Length - 1; i++)
+                    {
+                        nums[i] = nums[i + 1];
+                    }
+                    
+                    nums[nums.Length - 1] = num;
+                }
+
+                contador++;
+
+                if (contador == nums.Length)
+                    break;
+            }
+            
+            return qntUnico;
+        }
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,14 +1,12 @@
-﻿using LeetCode.Easy._21;
+﻿using LeetCode.Easy._26;
 class Program
 {
     public static void Main(string[] args)
     {
         var teste = new Solution();
+        int[] list = new int[] { 1, 1, 2};
 
-        var list1 = new ListNode(1, new ListNode(2, new ListNode(4)));
-        var list2 = new ListNode(1, new ListNode(3, new ListNode(4)));
-
-        Console.WriteLine(teste.MergeTwoLists(list1, list2));
+        Console.WriteLine(teste.RemoveDuplicates(list));
         Console.ReadLine();
     }
 }
