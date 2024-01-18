dd878d6f81b9407a3537e3f11c7656bca5dd0ed8
rmenchio <rodrigomenchio@id.uff.br>
2024-01-18 00:20:51 -0300

    primeira versão (usando ferramentas c#)

---
+++ LeetCode/Hard/4/Solution.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCode.Hard._4
+{
+    public class Solution
+    {
+        public double FindMedianSortedArrays(int[] nums1, int[] nums2)
+        {
+            double mediana = 0.0;
+
+            int[] nums3 = nums1.Concat(nums2).ToArray();
+
+            Array.Sort(nums3);
+
+            int tamanho = nums3.Length - 1;
+
+            if (tamanho % 2 != 0)
+            {
+                mediana = (nums3[tamanho / 2] + nums3[(tamanho / 2) + 1]) / 2.0;
+            }
+            else
+            {
+                mediana = nums3[tamanho / 2] / 1.0;
+            }
+
+            return mediana;
+        }
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,11 +1,13 @@
-﻿using LeetCode.Easy._70;
+﻿using LeetCode.Hard._4;
 class Program
 {
     public static void Main(string[] args)
     {
         var teste = new Solution();
+        int[] lista1 = { 1, 2 };
+        int[] lista2 = { 3, 4 };
 
-        Console.WriteLine(teste.ClimbStairs(4));
+        Console.WriteLine(teste.FindMedianSortedArrays(lista1, lista2));
         Console.ReadLine();
     }
 }
