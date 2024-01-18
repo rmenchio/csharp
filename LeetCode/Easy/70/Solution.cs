7b0b78fb9d2da5b3b917f3288243f0962e42fcf4
rmenchio <rodrigomenchio@id.uff.br>
2024-01-17 23:59:16 -0300

    primeira versão

---
+++ LeetCode/Easy/70/Solution.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCode.Easy._70
+{
+    public class Solution
+    {
+        public int ClimbStairs(int n)
+        {
+            int numeroAnterior = 0;
+            int numeroAtual = 1;
+            int possibilidades = 0;
+
+            for(int i = 0; i < n; i++)
+            {
+                possibilidades = numeroAnterior + numeroAtual;
+                numeroAnterior = numeroAtual;
+                numeroAtual = possibilidades;
+            }
+
+            return possibilidades;
+        }
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,12 +1,11 @@
-﻿using LeetCode.Easy._1207;
+﻿using LeetCode.Easy._70;
 class Program
 {
     public static void Main(string[] args)
     {
         var teste = new Solution();
-        int[] lista = { 1, 1, 3, 3, 3, };
 
-        Console.WriteLine(teste.UniqueOccurrences(lista));
+        Console.WriteLine(teste.ClimbStairs(4));
         Console.ReadLine();
     }
 }
