802dddbaac7bbb6d402d7f0db141d9fc22c3ce09
rmenchio <rodrigomenchio@id.uff.br>
2024-01-24 23:20:18 -0300

    solução

---
+++ LeetCode/Medium/1457/Solution.cs
@@ -0,0 +1,42 @@
+﻿namespace LeetCode.Medium._1457
+{
+    public class TreeNode {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+        }
+    }
+
+    public class Solution
+    {
+        public int PseudoPalindromicPaths(TreeNode raiz)
+        {
+            return ContarCaminhosPseudoPalindromo(raiz, 0);
+        }
+
+        private int ContarCaminhosPseudoPalindromo(TreeNode no, int contagem)
+        {
+            if (no == null)
+            {
+                return 0;
+            }
+
+            contagem ^= 1 << (no.val - 1);
+
+            if (no.left == null && no.right == null)
+            {
+                // Check if the current path is pseudo-palindromic
+                return (contagem & (contagem - 1)) == 0 ? 1 : 0;
+            }
+
+            int caminhosEsquerda = ContarCaminhosPseudoPalindromo(no.left, contagem);
+            int caminhosDireita = ContarCaminhosPseudoPalindromo(no.right, contagem);
+
+            return caminhosEsquerda + caminhosDireita;
+        }
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,11 +1,13 @@
-﻿using LeetCode.Medium._3;
+﻿using LeetCode.Medium._1457;
 class Program
 {
     public static void Main(string[] args)
     {
         var teste = new Solution();
 
-        Console.WriteLine(teste.LengthOfLongestSubstring(" "));
+        TreeNode root = new(2,new(3, new(3), new(1)), new(1, null, new(1)));
+
+        Console.WriteLine(teste.PseudoPalindromicPaths(root));
         Console.ReadLine();
     }
 }
