d16df8a07291c98b3acb9eededae2a856853df0f
rmenchio <rodrigomenchio@id.uff.br>
2024-01-30 21:49:51 -0300

    versão inicial

---
+++ LeetCode/Medium/739/Solution.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCode.Medium
+{
+    public class Solution
+    {
+        public int[] DailyTemperatures(int[] temperatures)
+        {
+            int quantidadeTemperaturas = temperatures.Length;
+            int[] diasAntesDoDiaMaisQuente = new int[quantidadeTemperaturas];
+
+            for(int i = 0; i < quantidadeTemperaturas; i++)
+            {
+                int contador = 1;
+
+                for (int j = i + 1; j < quantidadeTemperaturas; j++)
+                {
+                    if (temperatures[j] > temperatures[i])
+                    {
+                        diasAntesDoDiaMaisQuente[i] = contador;
+                        break;
+                    }
+                    else
+                    {
+                        contador++;
+                    }
+                }
+            }
+
+            return diasAntesDoDiaMaisQuente;
+        }
+    }
+}
