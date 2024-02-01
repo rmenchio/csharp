b12ae56ae5dcec946e0252511193b7ed1e464ad9
rmenchio <rodrigomenchio@id.uff.br>
2024-02-01 18:09:57 -0300

    versão inicial

---
+++ LeetCode/Medium/2966/Solution.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode.Medium._2966
+{
+    public class Solution
+    {
+        public int[][] DivideArray(int[] nums, int k)
+        {
+            Array.Sort(nums);
+
+            List<int[]> resultado = new();
+
+            for(int i = 0; i < nums.Length - 2; i += 3)
+            {
+                if (Math.Abs(nums[i] - nums[i + 1]) <= k && Math.Abs(nums[i] - nums[i + 2]) <= k)
+                    resultado.Add(new int[] { nums[i], nums[i + 1], nums[i + 2] });
+                else
+                    return new int[][] {};
+            }
+
+            return resultado.ToArray();
+        }
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,11 +1,11 @@
-﻿using LeetCode.Medium._150;
+﻿using LeetCode.Medium._2966;
 class Program
 {
     public static void Main(string[] args)
     {
         var teste = new Solution();
 
-        Console.WriteLine(teste.EvalRPN(new[] { "2", "1", "+", "3", "*" }));
+        Console.WriteLine(teste.DivideArray(new int[]{ 1, 3, 4, 8, 7, 9, 3, 5, 1}, 2));
         Console.ReadLine();
     }
 }
