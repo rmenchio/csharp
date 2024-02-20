2dc21c51435323ba1c60a5df0964ce1eea24a7f1
rmenchio <rodrigomenchio@id.uff.br>
2024-02-20 18:55:01 -0300

    primeira versão

---
+++ LeetCode/Easy/268/Solution.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCode.Easy._268
+{
+    public class Solution
+    {
+        public int MissingNumber(int[] nums)
+        {
+            Array.Sort(nums);
+
+            if (nums[0] != 0)
+                return 0;
+
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                if (nums[i] != nums[i + 1] - 1)
+                    return nums[i] + 1;
+            }
+
+            return nums[^1] + 1;
+        }
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,11 +1,11 @@
-﻿using LeetCode.Easy._231;
+﻿using LeetCode.Easy._268;
 class Program
 {
     public static void Main(string[] args)
     {
         var teste = new Solution();
 
-        Console.WriteLine(teste.IsPowerOfTwo(1));
+        Console.WriteLine(teste.MissingNumber(new int[]{1}));
         Console.ReadLine();
     }
 }
