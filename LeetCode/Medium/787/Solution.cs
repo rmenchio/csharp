c4024ba2820e271e251092cdae47ee03800a94e3
rmenchio <rodrigomenchio@id.uff.br>
2024-02-26 18:53:28 -0300

    versão final FindCheapestPrice

---
+++ LeetCode/Medium/787/Solution.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCode.Medium._787
+{
+    public class Solution
+    {
+        public int FindCheapestPrice(int n, int[][] flights, int src, int dst, int k)
+        {
+            int[] cost = new int[n];
+            Array.Fill(cost, int.MaxValue);
+            cost[src] = 0;
+
+            for (int i = 0; i <= k; i++)
+            {
+                int[] temp = (int[])cost.Clone();
+                foreach (var flight in flights)
+                {
+                    int from = flight[0];
+                    int to = flight[1];
+                    int price = flight[2];
+                    if (cost[from] != int.MaxValue && temp[to] > cost[from] + price)
+                    {
+                        temp[to] = cost[from] + price;
+                    }
+                }
+                cost = temp;
+            }
+
+            return cost[dst] == int.MaxValue ? -1 : cost[dst];
+        }
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,11 +1,19 @@
-﻿using LeetCode.Easy._268;
+﻿using LeetCode.Medium._787;
 class Program
 {
     public static void Main(string[] args)
     {
         var teste = new Solution();
 
-        Console.WriteLine(teste.MissingNumber(new int[]{1}));
+        int[][] flights = new int[][] {
+            new int[] { 0, 1, 100 },
+            new int[] { 1, 2, 100 },
+            new int[] { 2, 0, 100 },
+            new int[] { 1, 3, 600 },
+            new int[] { 2, 3, 200 }
+        };
+
+        Console.WriteLine(teste.FindCheapestPrice(4, flights, 0, 3, 1));
         Console.ReadLine();
     }
 }
