363b94326f241cf73e93a17d0f2d701355221eb4
rmenchio <rodrigomenchio@id.uff.br>
2024-03-19 14:27:30 -0300

    não finalizado ainda

---
+++ LeetCode/Medium/1171/Solution.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode.Medium._1171
+{
+    // Ainda não finalizei
+    public class Solution
+    {
+        public ListNode RemoveZeroSumSublists(ListNode head)
+        {
+            var refInicio = head;
+            List<int> lista = new();
+
+            while (refInicio != null)
+            {
+                lista.Add(refInicio.val);
+                refInicio = refInicio.next;
+            }
+
+            return new();
+        }
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,15 +1,14 @@
 ﻿
-using LeetCode.Easy._349;
+using LeetCode.Medium._1171;
 class Program
 {
     public static void Main(string[] args)
     {
         var teste = new Solution();
 
-        int[] l1 = { 1, 2, 2, 1 };
-        int[] l2 = { 2, 2 };
+        ListNode head = new(1, new(2, new(-3, new(3, new(1)))));
 
-        Console.WriteLine(teste.Intersection(l1, l2));
+        Console.WriteLine(teste.RemoveZeroSumSublists(head));
         Console.ReadLine();
     }
 }
