ef3295cc12e44d3214a30efc4a36742a65ac65c3
rmenchio <rodrigomenchio@id.uff.br>
2024-03-26 19:44:31 -0300

    finalizando 41. First Missing Positive

---
+++ LeetCode/Hard/41/Solution.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode.Hard._41
+{
+    public class Solution
+    {
+        public int FirstMissingPositive(int[] nums)
+        {
+            nums = nums
+                .Where(x => x > 0)
+                .Distinct()
+                .OrderBy(x => x)
+                .ToArray();
+
+            if (nums.Length == 0 || nums[0] != 1)
+                return 1;
+
+            for(int i = 0; i < nums.Length - 1; i++)
+            {
+                if (nums[i] + 1 != nums[i + 1])
+                    return nums[i] + 1;
+            }
+
+            return nums.Last() + 1;
+        }
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,12 +1,12 @@
 ﻿
-using LeetCode.Easy._2485;
+using LeetCode.Hard._41;
 class Program
 {
     public static void Main(string[] args)
     {
         var teste = new Solution();
 
-        Console.WriteLine(teste.PivotInteger(8));
+        Console.WriteLine(teste.FirstMissingPositive(new [] { 1,2,0 } ));
         Console.ReadLine();
     }
 }
