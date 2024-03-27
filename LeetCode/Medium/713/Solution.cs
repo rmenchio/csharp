1286800f71bc7b1e512c136fd29e80a017a6a4e9
rmenchio <rodrigomenchio@id.uff.br>
2024-03-27 11:28:30 -0300

    primeira versão - 713. Subarray Product Less Than K

---
+++ LeetCode/Medium/713/Solution.cs
@@ -0,0 +1,47 @@
+﻿namespace LeetCode.Medium._713
+{
+    public class Solution
+    {
+        public int NumSubarrayProductLessThanK(int[] nums, int k)
+        {
+            int numeroSubArrays = 0;
+
+            int posicaoInicial = 0;
+
+            int i = 1;
+
+            int multiplicacao = nums[posicaoInicial];
+
+            while (posicaoInicial < nums.Length)
+            {
+                if (multiplicacao < k)
+                {
+                    numeroSubArrays++;
+
+                    if (i < nums.Length)
+                    {
+                        multiplicacao *= nums[i];
+                        i++;
+                    }
+                    else
+                    {
+                        multiplicacao = k;
+                    }
+                }
+                else
+                {
+                    posicaoInicial++;
+
+                    if (posicaoInicial >= nums.Length)
+                        break;
+
+                    multiplicacao = nums[posicaoInicial];
+
+                    i = posicaoInicial + 1;
+                }
+            }
+
+            return numeroSubArrays;
+        }
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,12 +1,12 @@
 ﻿
-using LeetCode.Hard._41;
+using LeetCode.Medium._713;
 class Program
 {
     public static void Main(string[] args)
     {
         var teste = new Solution();
 
-        Console.WriteLine(teste.FirstMissingPositive(new [] { 1,2,0 } ));
+        Console.WriteLine(teste.NumSubarrayProductLessThanK(new [] { 1, 1 , 1}, 2 ));
         Console.ReadLine();
     }
 }
