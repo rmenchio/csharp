74a2799bc9aa9041091a7e67c1eae9268b993719
rmenchio <rodrigomenchio@id.uff.br>
2024-04-02 18:19:24 -0300

    finalizando - 205. Isomorphic Strings

---
+++ LeetCode/Easy/205/Solution.cs
@@ -0,0 +1,34 @@
+﻿namespace LeetCode.Easy._205
+{
+	public class Solution
+	{
+        public bool IsIsomorphic(string s, string t)
+        {
+            Dictionary<string, string> mapeamento = new();
+
+            for(int i = 0; i< s.Length; i++)
+            {
+                var letraS = s[i].ToString();
+                var letraT = t[i].ToString();
+
+                if (mapeamento.ContainsKey(letraS))
+                {
+                    var valor = mapeamento[letraS];
+
+                    if (!valor.Equals(letraT))
+                        return false;
+                }
+                else
+                {
+                    if (mapeamento.ContainsValue(letraT))
+                        return false;
+
+                    mapeamento.Add(letraS, letraT);
+                }
+                    
+            }
+
+            return true;
+        }
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,12 +1,12 @@
 ﻿
-using LeetCode.Medium._713;
+using LeetCode.Easy._205;
 class Program
 {
     public static void Main(string[] args)
     {
         var teste = new Solution();
 
-        Console.WriteLine(teste.NumSubarrayProductLessThanK(new [] { 1, 1 , 1}, 2 ));
+        Console.WriteLine(teste.IsIsomorphic("egg", "add"));
         Console.ReadLine();
     }
 }
