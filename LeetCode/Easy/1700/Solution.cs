5ada2e13898de7fbe719c0c9eada7d7d904cb44e
rmenchio <rodrigomenchio@id.uff.br>
2024-04-08 18:10:10 -0300

    finalizando desafio de hoje - 1700. Number of Students Unable to Eat Lunch

---
+++ LeetCode/Easy/1700/Solution.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetCode.Easy._1700
+{
+    // 1,1,1 (estudantes)
+    // 0,1,1 (sanduiches)
+    public class Solution
+    {
+        public int CountStudents(int[] students, int[] sandwiches)
+        {
+            Queue<int> estudantes = new(students);
+            Stack<int> sanduiches = new(sandwiches.Reverse());
+
+            var tamanhoInicial = students.Length * students.Length;
+
+            for (int i = 0; i < tamanhoInicial; i++)
+            {
+                if (sanduiches.Count.Equals(0))
+                    break;
+
+                if (estudantes.Peek().Equals(sanduiches.Peek()))
+                {
+                    estudantes.Dequeue();
+                    sanduiches.Pop();
+                }
+                else
+                {
+                    estudantes.Enqueue(estudantes.Dequeue());
+                }
+            }
+
+            return estudantes.Count;
+        }
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,12 +1,12 @@
 ﻿
-using LeetCode.Easy._205;
+using LeetCode.Easy._1700;
 class Program
 {
     public static void Main(string[] args)
     {
         var teste = new Solution();
 
-        Console.WriteLine(teste.IsIsomorphic("egg", "add"));
+        Console.WriteLine(teste.CountStudents(new [] {1,1,1,0,0,1}, new[] {1,0,0,0,1,1}));
         Console.ReadLine();
     }
 }
