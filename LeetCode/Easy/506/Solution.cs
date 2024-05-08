deb4e2f6d81a3a47c85b31f7f5cd7e7a7dd58b2b
rmenchio <rodrigomenchio@id.uff.br>
2024-05-08 18:36:04 -0300

    finalizando 08/05/2024 - 506. Relative Ranks

---
+++ LeetCode/Easy/506/Solution.cs
@@ -0,0 +1,45 @@
+﻿namespace LeetCode.Easy._506
+{
+    public class Solution
+    {
+        public string[] FindRelativeRanks(int[] score)
+        {
+            var listaOrdenada = new List<int>(score);
+
+            listaOrdenada.Sort();
+            listaOrdenada.Reverse();
+
+            Dictionary<int, string> relacao = new();
+
+            int posicao = 1;
+
+
+            foreach (int i in listaOrdenada)
+            {
+                relacao.Add(i, MapeandoPosicao(posicao));
+                posicao++;
+            }
+
+            List<string> listaFinal = new();
+
+            foreach(var i in score)
+            {
+                relacao.TryGetValue(i, out string value);
+
+                listaFinal.Add(value);
+            }
+
+            return listaFinal.ToArray();
+            
+        }
+
+        private static string MapeandoPosicao(int posicao) => posicao switch
+        {
+            1 => "Gold Medal",
+            2 => "Silver Medal",
+            3 => "Bronze Medal",
+            _ => posicao.ToString()
+
+        };
+    }
+}
