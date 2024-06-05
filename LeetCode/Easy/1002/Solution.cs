d95e44959c20450955c2785114f2d5a078dd73bd
rmenchio <rodrigomenchio@id.uff.br>
2024-06-05 10:55:51 -0300

    solução 1002. Find Common Characters

---
+++ LeetCode/Easy/1002/Solution.cs
@@ -0,0 +1,40 @@
+﻿namespace LeetCode.Easy._1002
+{
+    public class Solution
+    {
+        public IList<string> CommonChars(string[] words)
+        {
+            List<string> caracteres = new();
+
+            var primeiraPalavra = words[0];
+            var listaPalavras = words.ToList();
+            listaPalavras.Remove(primeiraPalavra);
+
+            foreach (char letra in primeiraPalavra)
+            {
+                bool contem = true;
+
+                for (int i = 0; i < listaPalavras.Count; i++)
+                {
+                    if (!listaPalavras[i].Contains(letra))
+                    {
+                        contem = false;
+                        break;
+                    }
+
+                    int index = listaPalavras[i].IndexOf(letra);
+
+                    if (index != -1)
+                    {
+                        listaPalavras[i] = listaPalavras[i].Remove(index, 1);
+                    }
+                }
+
+                if (contem)
+                    caracteres.Add(letra.ToString());
+            }
+
+            return caracteres;
+        }
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,12 +1,12 @@
 ﻿
-using LeetCode.Easy._1897;
+using LeetCode.Easy._1002;
 class Program
 {
     public static void Main(string[] args)
     {
         var teste = new Solution();
 
-        Console.WriteLine(teste.MakeEqual(new[] { "caaaaa", "aaaaaaaaa", "a", "bbb", "bbbbbbbbb", "bbb", "cc", "cccccccccccc", "ccccccc", "ccccccc", "cc", "cccc", "c", "cccccccc", "c" }));
+        Console.WriteLine(teste.CommonChars(new[] { "bella", "label", "roller" }));
         Console.ReadLine();
     }
 }
