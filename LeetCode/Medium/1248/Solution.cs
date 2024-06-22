652612f438e48f124b589e65b413985f18c6479c
rmenchio <rodrigomenchio@id.uff.br>
2024-06-22 17:10:16 -0300

    finalizando 1248. Count Number of Nice Subarrays

---
+++ LeetCode/Medium/1248/Solution.cs
@@ -0,0 +1,40 @@
+﻿namespace LeetCode.Medium._1248
+{
+    public class Solution
+    {
+        public int NumberOfSubarrays(int[] nums, int k)
+        {
+            int numeroSubArrays = 0;
+            int n = nums.Length;
+            int countOdd = 0;
+            var prefixCounts = new Dictionary<int, int>();
+            prefixCounts[0] = 1;
+
+            int currentSum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                if (nums[i] % 2 != 0)
+                {
+                    countOdd++;
+                }
+
+                currentSum = countOdd;
+
+                if (prefixCounts.ContainsKey(currentSum - k))
+                {
+                    numeroSubArrays += prefixCounts[currentSum - k];
+                }
+
+                if (!prefixCounts.ContainsKey(currentSum))
+                {
+                    prefixCounts[currentSum] = 0;
+                }
+
+                prefixCounts[currentSum]++;
+            }
+
+            return numeroSubArrays;
+        }
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,15 +1,12 @@
 ﻿
-using LeetCode.Medium._826;
+using LeetCode.Medium._1248;
 class Program
 {
     public static void Main(string[] args)
     {
         var teste = new Solution();
-        var difficulty = new[] { 2, 4, 6, 8, 10 };
-        var profit = new[] { 10, 20, 30, 40, 50 };
-        var worker = new[] { 4, 5, 6, 7 };
 
-        Console.WriteLine(teste.MaxProfitAssignment(difficulty, profit, worker));
+        Console.WriteLine(teste.NumberOfSubarrays(new[] { 2, 2, 2, 1, 2, 2, 1, 2, 2, 2 }, 2));
         Console.ReadLine();
     }
 }
