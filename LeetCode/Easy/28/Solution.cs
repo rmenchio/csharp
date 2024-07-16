be2ac972c51b4ab01ff77f2b9e7d7aab98496297
rmenchio <rodrigomenchio@id.uff.br>
2024-07-16 16:04:29 -0300

    finalizando 28. Find the Index of the First Occurrence in a String

---
+++ LeetCode/Easy/28/Solution.cs
@@ -0,0 +1,40 @@
+﻿namespace LeetCode.Easy._28
+{
+    public class Solution
+    {
+        public int StrStr(string haystack, string needle)
+        {
+            int tamanhoMaximoNeedle = needle.Length;
+            int tamanhoMaximoHaystack = haystack.Length;
+            int contador = 1;
+            var primeiroCaracter = needle[0];
+
+            for (int i = 0; i < haystack.Length; i++)
+            {
+                if (haystack[i].Equals(primeiroCaracter) && tamanhoMaximoHaystack - i >= tamanhoMaximoNeedle)
+                {
+                    if (needle.Length == 1)
+                        return i;
+
+                    for(int j = 1; j < needle.Length; j++)
+                    {
+                        if (!haystack[i + j].Equals(needle[j]))
+                        {
+                            contador = 1;
+                            break;
+                        }
+                        else
+                        {
+                            contador++;
+                        }
+
+                        if (contador == tamanhoMaximoNeedle)
+                            return i;
+                    }
+                }
+            }
+
+            return -1;
+        }
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,12 +1,12 @@
 ﻿
-using LeetCode.Medium._1248;
+using LeetCode.Easy._28;
 class Program
 {
     public static void Main(string[] args)
     {
         var teste = new Solution();
 
-        Console.WriteLine(teste.NumberOfSubarrays(new[] { 2, 2, 2, 1, 2, 2, 1, 2, 2, 2 }, 2));
+        Console.WriteLine(teste.StrStr("leetcode", "leeto"));
         Console.ReadLine();
     }
 }
