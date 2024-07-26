40d0f1b556bbb43859af4908181f1fa7ee7f35f1
rmenchio <rodrigomenchio@id.uff.br>
2024-07-25 22:06:55 -0300

    finalizando 67. Add Binary

---
+++ LeetCode/Easy/67/Solution.cs
@@ -0,0 +1,64 @@
+﻿namespace LeetCode.Easy._67
+{
+    public class Solution
+    {
+        public string AddBinary(string a, string b)
+        {
+            var tamanhoA = a.Length - 1;
+            var tamanhoB = b.Length - 1;
+
+            bool sobrou = false;
+
+            Stack<string> resultado = new();
+
+            while (tamanhoA >= 0 || tamanhoB >= 0)
+            {
+                int atualA = 0;
+                int atualB = 0;
+
+                if (tamanhoA >= 0)
+                {
+                    atualA = a[tamanhoA] - '0';
+                    tamanhoA--;
+                }
+
+                if (tamanhoB >= 0)
+                {
+                    atualB = b[tamanhoB] - '0';
+                    tamanhoB--;
+                }
+
+                var soma = atualA + atualB;
+
+                if (sobrou)
+                    soma++;
+
+                switch (soma)
+                {
+                    case 0:
+                        resultado.Push("0");
+                        sobrou = false;
+                        break;
+                    case 1:
+                        resultado.Push("1");
+                        sobrou = false;
+                        break;
+                    case 2:
+                        resultado.Push("0");
+                        sobrou = true;
+                        break;
+                    case 3:
+                        resultado.Push("1");
+                        sobrou = true;
+                        break;
+                };
+
+            }
+
+            if (sobrou)
+                resultado.Push("1");
+
+            return string.Join("", resultado.ToArray());
+        }
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,12 +1,12 @@
 ﻿
-using LeetCode.Easy._66;
+using LeetCode.Easy._67;
 class Program
 {
     public static void Main(string[] args)
     {
         var teste = new Solution();
 
-        Console.WriteLine(teste.PlusOne(new[]{ 8,9,9,9}));
+        Console.WriteLine(teste.AddBinary("11", "1"));
         Console.ReadLine();
     }
 }
