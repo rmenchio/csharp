6bc8de3037ac2e39133b997f656de420a7eecb85
rmenchio <rodrigomenchio@id.uff.br>
2024-07-29 16:07:10 -0300

    resolvendo 69. Sqrt(x)

---
+++ LeetCode/Easy/69/Solution.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCode.Easy._69
+{
+    public class Solution
+    {
+        // OBSERVAÇÃO IMPORTANTE
+        // You must not use any built-in exponent function or operator.
+        // For example, do not use pow(x, 0.5) in c++ or x ** 0.5 in python.
+
+        public int MySqrt(int x)
+        {
+            if (x == 0) return 0;
+            int left = 1, right = x, result = 0;
+
+            while (left <= right)
+            {
+                int mid = left + (right - left) / 2;
+                if (mid <= x / mid)
+                {
+                    left = mid + 1;
+                    result = mid;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            }
+
+            return result;
+        }
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,12 +1,12 @@
 ﻿
-using LeetCode.Easy._67;
+using LeetCode.Easy._69;
 class Program
 {
     public static void Main(string[] args)
     {
         var teste = new Solution();
 
-        Console.WriteLine(teste.AddBinary("11", "1"));
+        Console.WriteLine(teste.MySqrt(2147483647));
         Console.ReadLine();
     }
 }
