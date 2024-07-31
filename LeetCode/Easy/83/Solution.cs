e00264495f50e450c99d09ab2628f18737b9ffca
rmenchio <rodrigomenchio@id.uff.br>
2024-07-31 18:17:28 -0300

    finalizando 83. Remove Duplicates from Sorted List

---
+++ LeetCode/Easy/83/Solution.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode.Easy._83
+{
+    public class Solution
+    {
+        public ListNode DeleteDuplicates(ListNode head)
+        {
+            var aux = head;
+
+            while(aux != null)
+            {
+                var proximo = aux?.next;
+
+                while (proximo != null && aux.val == proximo.val)
+                {
+                    aux.next = proximo.next;
+                    proximo = aux.next;
+                }
+
+                aux = aux.next;
+            }
+
+            return head;
+        }
+    }
+}
