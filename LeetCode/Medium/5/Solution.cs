e7855dc1d3d982364069975bb0879fa7179ae991
rmenchio <rodrigomenchio@id.uff.br>
2024-08-09 17:35:49 -0300

    finalizando 5. Longest Palindromic Substring

---
+++ LeetCode/Medium/5/Solution.cs
@@ -0,0 +1,37 @@
+﻿namespace LeetCode.Medium._5
+{
+    public class Solution
+    {
+        public string LongestPalindrome(string s)
+        {
+            if (string.IsNullOrEmpty(s)) return "";
+
+            int start = 0, maxLength = 1;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                int len1 = ExpandAroundCenter(s, i, i); // Palíndromos de comprimento ímpar
+                int len2 = ExpandAroundCenter(s, i, i + 1); // Palíndromos de comprimento par
+
+                int len = Math.Max(len1, len2);
+                if (len > maxLength)
+                {
+                    start = i - (len - 1) / 2;
+                    maxLength = len;
+                }
+            }
+
+            return s.Substring(start, maxLength);
+        }
+
+        private int ExpandAroundCenter(string s, int left, int right)
+        {
+            while (left >= 0 && right < s.Length && s[left] == s[right])
+            {
+                left--;
+                right++;
+            }
+            return right - left - 1; // Tamanho do palíndromo
+        }
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,12 +1,12 @@
 ﻿
-using LeetCode.Easy._222;
+using LeetCode.Medium._5;
 class Program
 {
     public static void Main(string[] args)
     {
         var teste = new Solution();
 
-        Console.WriteLine(teste.CountNodes(null));
+        Console.WriteLine(teste.LongestPalindrome("bacabab"));
         Console.ReadLine();
     }
 }
