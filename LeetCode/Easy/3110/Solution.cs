a27b3dba9a278aca2a7d9130fb127bc6bdcb2f8a
rmenchio <rodrigomenchio@id.uff.br>
2024-09-03 10:57:36 -0300

    resolvendo 3110. Score of a String

---
+++ LeetCode/Easy/3110/Solution.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCode.Easy._3110
+{
+    public class Solution
+    {
+        public int ScoreOfString(string s)
+        {
+            int soma = 0;
+
+            for(int i = 0; i < s.Length - 1; i++)
+            {
+                soma += Math.Abs(s[i] - s[i + 1]);
+            }
+
+            return soma;
+        }
+    }
+}
